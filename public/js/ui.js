0d4ad8cb162c0a09205468831d2ecc6f56251611
Daniel St. Jules <danielst.jules@gmail.com>
2013-05-30 16:46:01 -0400

    Add and Next buttons working, as well as automatically playing next song in Queue. Play Now not working

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,17 +1,26 @@
 // Use Ajax so that page refreshes aren't necessary when using the UI
 
+var lastClick = 0;
+
 $(document).click(function(event) {
     // Prevent default click behaviour for all links unless they have the class
     // 'outbound'. In that case we assume it's a link to a page outside of
     // SoundStation and should proceed normally
     if (!$(event.target).hasClass('outbound')) {
       event.preventDefault();
-      var href = $(event.target).attr('href');
-      // Perform ajax request
-      $.ajax({
-        url: href,
-        cache: false
-      });
+
+      // Also prevent the user from clicking too quickly
+      var now = new Date().getTime();
+      if (now - lastClick > 400) {
+        var href = $(event.target).attr('href');
+        // Perform ajax request
+        $.ajax({
+          url: href,
+          cache: false
+        });
+        lastClick = now;
+      }
+
       return false;
     }
 });
